4f5f17471d1be18056b6c274f405d5d460248a64
brainwithE <haylyzh@gmail.com>
2016-07-12 13:30:36 +0800

    Potatocodes Main Website First Iteration

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+
+		compass: {
+			dist: {
+				options: {
+					sassDir: 'assets/css',
+					cssDir: 'assets/css'
+				}
+			}
+		},
+
+		watch: {
+			stylesheets: {
+				files: '**/*.scss',
+				tasks: 'compass'
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+
+	grunt.registerTask('default', ['watch']);
+};
